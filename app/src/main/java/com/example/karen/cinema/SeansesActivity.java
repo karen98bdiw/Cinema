1712a34add02fe5d951f8b149e32961597ea8b27
karen <ghazaryan.98@mail.ru>
2018-12-09 17:48:08 +0200

    can reserve place and create user

---
+++ app/src/main/java/com/example/karen/cinema/SeansesActivity.java
@@ -0,0 +1,40 @@
+package com.example.karen.cinema;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeansesActivity extends AppCompatActivity {
+
+    private List<Seans> data;
+    private RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seanses);
+        data = new ArrayList<>();
+
+        Intent intent = getIntent();
+        Film film = (Film) intent.getSerializableExtra("currentFilm");
+        data = film.getSeanses();
+
+        recyclerView = findViewById(R.id.seansRecycle);
+
+        LinearLayoutManager manager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+
+
+
+        final SeansesAdapter adapter = new SeansesAdapter(data,SeansesActivity.this);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
